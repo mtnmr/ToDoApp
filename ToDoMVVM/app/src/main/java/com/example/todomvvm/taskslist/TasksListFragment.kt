8ba742aec1ec048659611c2efd346a435e118b17
mtnmr <mei.rila1013@gmail.com>
2022-08-10 16:22:47 +0900

    taskFilter ui

--- ToDoMVVM/app/src/main/java/com/example/todomvvm/taskslist/TasksListFragment.kt
+++ ToDoMVVM/app/src/main/java/com/example/todomvvm/taskslist/TasksListFragment.kt
@@ -1,11 +1,14 @@
 package com.example.todomvvm.taskslist
 
 import android.os.Bundle
+import android.view.*
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.todomvvm.R
@@ -42,11 +45,49 @@ class TasksListFragment : Fragment() {
             viewModel = tasksViewModel
         }
 
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.taks_list_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when(menuItem.itemId){
+                    R.id.menu_filter -> {
+//                        Toast.makeText(requireContext(), "menu item selected", Toast.LENGTH_SHORT).show()
+                        showFilterPopUpMenu()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
     }
 
     fun nextFragment(){
         val action = TasksListFragmentDirections.actionTasksListFragmentToTaskAddEditFragment()
         findNavController().navigate(action)
+    }
+
+    fun showFilterPopUpMenu(){
+        val view = activity?.findViewById<View>(R.id.menu_filter)
+        PopupMenu(requireContext(), view).run {
+            menuInflater.inflate(R.menu.task_filter_menu, menu)
+
+            setOnMenuItemClickListener {
+                tasksViewModel.taskFilterChange(
+                    when(it.itemId){
+                        R.id.completed_task -> TaskFilter.COMPLETED_TASK
+                        R.id.active_task -> TaskFilter.ACTIVE_TASK
+                        else -> TaskFilter.ALL_TASK
+                    }
+                )
+                true
+            }
+
+            show()
+        }
     }
 
 }
--- ToDoMVVM/app/src/main/java/com/example/todomvvm/taskslist/TasksViewModel.kt
+++ ToDoMVVM/app/src/main/java/com/example/todomvvm/taskslist/TasksViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.todomvvm.taskslist
 
+
 import androidx.lifecycle.*
+import com.example.todomvvm.R
 import com.example.todomvvm.data.Task
 import com.example.todomvvm.data.TaskRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,6 +12,9 @@ import javax.inject.Inject
 @HiltViewModel
 class TasksViewModel @Inject constructor(private val repository: TaskRepository) : ViewModel() {
 
+    private var _taskFilter = MutableLiveData<Int>(R.string.all_task)
+    val taskFilter:LiveData<Int> = _taskFilter
+
     val allTasks : LiveData<List<Task>> = repository.allTask.asLiveData()
 
     fun updateChecked(id:Int, isChecked:Boolean){
@@ -17,6 +22,21 @@ class TasksViewModel @Inject constructor(private val repository: TaskRepository)
             repository.updateChecked(id, isChecked)
         }
     }
+
+    fun taskFilterChange(taskFilter: TaskFilter){
+        _taskFilter.value =
+            when(taskFilter){
+                TaskFilter.ALL_TASK -> R.string.all_task
+                TaskFilter.COMPLETED_TASK -> R.string.completed_task
+                TaskFilter.ACTIVE_TASK -> R.string.active_task
+            }
+    }
+}
+
+enum class TaskFilter{
+    ALL_TASK,
+    COMPLETED_TASK,
+    ACTIVE_TASK
 }
 
 val sampleList = listOf(
