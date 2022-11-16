4bd2abe9b87c92f6f9523c5ca8783454e6f8aaaa
mtnmr <mei.rila1013@gmail.com>
2022-11-16 10:59:31 +0900

    tasksViewModel taskFilterTest

--- ToDoMVVM/app/src/test/java/com/example/todomvvm/TasksViewModelTest.kt
+++ ToDoMVVM/app/src/test/java/com/example/todomvvm/TasksViewModelTest.kt
@@ -1,14 +1,39 @@
 package com.example.todomvvm
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.example.todomvvm.taskslist.TaskFilter
 import com.example.todomvvm.taskslist.TasksViewModel
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
 
 class TasksViewModelTest {
 
     lateinit var tasksViewModel: TasksViewModel
 
+    @get:Rule
+    val rule: TestRule = InstantTaskExecutorRule()
+
     @Before
     fun setup(){
         tasksViewModel = TasksViewModel(FakeRepository())
+    }
+
+    @Test
+    fun taskFilterChange_allTask_to_activeTask(){
+        val observer = Observer<Int>{}
+
+        try {
+            tasksViewModel.taskFilter.observeForever(observer)
+            tasksViewModel.taskFilterChange(TaskFilter.ACTIVE_TASK)
+
+            val value = tasksViewModel.taskFilter.value
+            assertEquals(value, R.string.active_task)
+        }finally {
+            tasksViewModel.taskFilter.removeObserver(observer)
+        }
     }
 }
