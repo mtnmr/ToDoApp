376c0769af8e15c3e7745d2857521b172adc7ec0
mtnmr <mei.rila1013@gmail.com>
2022-11-25 10:33:03 +0900

    FakeRepository add feature, TasksListFragmentTest fix

--- ToDoMVVM/app/src/androidTest/java/com/example/todomvvm/TasksListFragmentTest.kt
+++ ToDoMVVM/app/src/androidTest/java/com/example/todomvvm/TasksListFragmentTest.kt
@@ -9,9 +9,14 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.todomvvm.data.ITaskRepository
+import com.example.todomvvm.data.Task
 import com.example.todomvvm.taskslist.TasksListAdapter
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -31,13 +36,23 @@ class TasksListFragmentTest {
     @Inject
     lateinit var repository: ITaskRepository
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Before
     fun init(){
         hiltRule.inject()
+        runTest(UnconfinedTestDispatcher()) {
+            repository.insertTask(Task(id=1, title = "task1", isChecked = false))
+            repository.insertTask(Task(id=2, title = "task2", isChecked = false))
+            repository.insertTask(Task(id=3, title = "task3", isChecked = true))
+            repository.insertTask(Task(id=4, title = "task4", isChecked = true))
+            repository.insertTask(Task(id=5, title = "task5", isChecked = true))
+
+            delay(1000)
+        }
     }
 
     @Test
-    fun taskList_isShow(){
+    fun taskList_isShow() {
         onView(withText("task1")).check(matches(isDisplayed()))
         onView(withText("task2")).check(matches(isDisplayed()))
         onView(withText("task3")).check(matches(isDisplayed()))
@@ -46,7 +61,7 @@ class TasksListFragmentTest {
     }
 
     @Test
-    fun taskFilterChangeActive_showOnlyActiveTask(){
+    fun taskFilterChangeActive_showOnlyActiveTask() {
         onView(withId(R.id.menu_filter)).perform(click())
         onView(withText("Active Tasks")).perform(click())
 
@@ -59,7 +74,8 @@ class TasksListFragmentTest {
     }
 
     @Test
-    fun taskFilterChangeCompleted_showOnlyCompletedTask(){
+    fun taskFilterChangeCompleted_showOnlyCompletedTask() {
+
         onView(withId(R.id.menu_filter)).perform(click())
         onView(withText("Completed Tasks")).perform(click())
 
@@ -88,5 +104,20 @@ class TasksListFragmentTest {
             R.id.task_checkbox,    // 見つけたいビューの ID（ここでは追加ボタン）
             0         // リストアイテムの位置
         )).check(matches(isChecked()))
+
+        onView(withId(R.id.menu_filter)).perform(click())
+        onView(withText("Completed Tasks")).perform(click())
+
+        Thread.sleep(2000)
+
+        onView(withText("task1")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun addTaskButtonClick_NavigateAddEditFragment(){
+        onView(withId(R.id.add_task_button)).perform(click())
+
+        onView(withId(R.id.task_title_edit)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_description_edit)).check(matches(isDisplayed()))
     }
 }
--- ToDoMVVM/app/src/androidTest/java/com/example/todomvvm/di/FakeRepository.kt
+++ ToDoMVVM/app/src/androidTest/java/com/example/todomvvm/di/FakeRepository.kt
@@ -3,37 +3,45 @@ package com.example.todomvvm.di
 import com.example.todomvvm.data.ITaskRepository
 import com.example.todomvvm.data.Task
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 class FakeRepository @Inject constructor():ITaskRepository {
 
-//    val items = mutableListOf<Task>()
+    val items = mutableListOf<Task>()
 
-    override val allTask: Flow<List<Task>> = flowOf(
-        listOf(
-            Task(id=1, title = "task1", isChecked = false),
-            Task(id=2, title = "task2", isChecked = false),
-            Task(id=3, title = "task3", isChecked = true),
-            Task(id=4, title = "task4", isChecked = true),
-            Task(id=5, title = "task5", isChecked = true),
-        )
-    )
+    private val flow = MutableStateFlow<List<Task>>(emptyList())
+
+    override val allTask: Flow<List<Task>> = flow
+//        flowOf(
+//        listOf(
+//            Task(id=1, title = "task1", isChecked = false),
+//            Task(id=2, title = "task2", isChecked = false),
+//            Task(id=3, title = "task3", isChecked = true),
+//            Task(id=4, title = "task4", isChecked = true),
+//            Task(id=5, title = "task5", isChecked = true),
+//        )
+//    )
 
     override fun getTask(id: Int): Flow<Task> {
-        return if(id == 1){
-            flowOf(Task(id = 1, title = "test task"))
-        }else{
-            flowOf(Task(id = 2, title = "test task2"))
-        }
+        return flowOf( items.find { it.id == id } ?: Task(id = -1, title = "Default Task") )
     }
 
     override suspend fun updateChecked(id: Int, isChecked: Boolean) {
-
+        val newItems:List<Task> = items.map {
+            if(it.id == id){
+                Task(id = it.id, title = it.title, description = it.description, isChecked = isChecked)
+            }else{
+                it
+            }
+        }
+        flow.emit(newItems)
     }
 
     override suspend fun insertTask(task: Task) {
-        TODO("Not yet implemented")
+        items.add(task)
+        flow.emit(items)
     }
 
     override suspend fun updateTask(task: Task) {
